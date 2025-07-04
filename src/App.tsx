49e3c94054ac08f63d7787bfbf74ae2bac66cae0
Lucas Costa <lucas.rccg@gmail.com>
2025-07-03 23:07:43 -0300

    reordering components in App - just rendering page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
+import { Home } from "./pages/Home";
 import "./styles/themes.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
-// import { Heading } from "./components/Heading";
-// import { TimerIcon } from "lucide-react";
 
-// <div>
-//   <Heading>
-//     <button>
-//       <TimerIcon />
-//     </button>
-//   </Heading>
-// </div>
 function App() {
-  return (
-    <>
-      <Container>
-        <Heading>Logo</Heading>
-      </Container>
-
-      <Container>
-        <Heading>Menu</Heading>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export { App };
